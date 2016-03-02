71adf2612980722c37c2af1965b456947f77e7d6
Adam Lif <adamlnk@neb.rr.com>
2016-03-02 17:56:09 -0600

    nav bar

--- CSCE378/index.php
+++ CSCE378/index.php
@@ -2,4 +2,23 @@
 require_once('core.php');
 html_top();
 ?>
-<h1>PHP is still working!</h1>
+<div class="container">
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Time Tracker</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Submit Correction</a></li>
+        <li><a href="#">Pay Calculator</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Hello, Username</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <h1>PHP is still working!</h1>
+  
+</div>
