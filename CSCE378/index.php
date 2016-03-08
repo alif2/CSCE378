4366dc0c8e561c7008419de7ec027413592f1935
Adam Lif <adamlnk@neb.rr.com>
2016-03-08 17:20:35 -0600

    Format page

--- CSCE378/index.php
+++ CSCE378/index.php
@@ -2,41 +2,11 @@
 require_once('core.php');
 html_top();
 ?>
-
-<button type="button" class="btn btn-default btnlg">CLOCK IN</button>
-<h2>Work Hour(s)</h2>
-<table class="table-bordered ptable">
-	<thead>
-  		<tr>
-  			<th>Date</th>
-  			<th>Monday	Feb.2</th>
-  			<th>Tuesday	Feb.3</th>
-  			<th>Wednesday	Feb.4</th>
-  			<th>Thursday	Feb.5</th>
-  			<th>Friday	Feb.6</th>
-  			<th>Saturday	Feb.7</th>
-  			<th>Sunday	Feb.8</th>
-  		</tr>
-  	</thead>
-  	<tbody>
-  		<tr>
-  			<td>Duration	[hour(s)]</td>
-  			<td>5.2</td>
-  			<td>4.3</td>
-  			<td></td>
-  			<td></td>
-  			<td></td>
-  			<td></td>
-  			<td></td>
-  		</tr>
-  	</tbody>
-  </table>
-  
 <div class="row">
   <div class="col-md-4">
+    <button type="button" class="btn btn-default btnlg">CLOCK IN</button>
   </div>
-  
-<div class="col-md-8">
+  <div class="col-md-8">
     Current Time:<p id="clock"></p>
     <?php
     if(database_get_user_clock_status(1) == 'ClockIn') {
@@ -46,6 +16,37 @@ html_top();
         echo 'Working Hours: ' . round($i_work_hours, 2) . '<br>'; 
     }
     ?>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <h2>Work Hour(s)</h2>
+    <table class="table-bordered ptable">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Monday Feb.2</th>
+          <th>Tuesday Feb.3</th>
+          <th>Wednesday Feb.4</th>
+          <th>Thursday Feb.5</th>
+          <th>Friday Feb.6</th>
+          <th>Saturday Feb.7</th>
+          <th>Sunday Feb.8</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Duration [hour(s)]</td>
+          <td>5.2</td>
+          <td>4.3</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          </tr>
+      </tbody>
+    </table>
   </div>
 </div>
 <?
