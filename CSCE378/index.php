91ca170b325ca8a1050665aa3cbdd56f8b9f9d18
Mengmei Ye <mengmeiye@gmail.com>
2016-03-08 16:46:24 -0600

    Button and Table

--- CSCE378/index.php
+++ CSCE378/index.php
@@ -19,6 +19,35 @@ html_top();
     </div>
   </nav>
   
+  <button type="button" class="btn btn-default btnlg">CLOCK IN</button>
+  <h2>Work Hour(s)</h2>
+  <table class="table-bordered ptable">
+  	<thead>
+  		<tr>
+  			<th>Date</th>
+  			<th>Monday	Feb.2</th>
+  			<th>Tuesday	Feb.3</th>
+  			<th>Wednesday	Feb.4</th>
+  			<th>Thursday	Feb.5</th>
+  			<th>Friday	Feb.6</th>
+  			<th>Saturday	Feb.7</th>
+  			<th>Sunday	Feb.8</th>
+  		</tr>
+  	</thead>
+  	<tbody>
+  		<tr>
+  			<td>Duration	[hour(s)]</td>
+  			<td>5.2</td>
+  			<td>4.3</td>
+  			<td></td>
+  			<td></td>
+  			<td></td>
+  			<td></td>
+  			<td></td>
+  		</tr>
+  	</tbody>
+  </table>
+  
   <div class="row">
     <div class="col-md-4">
     </div>
