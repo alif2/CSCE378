5471351350794c5b78bd9baa4cfd54b309af793a
Adam Lif <adamlnk@neb.rr.com>
2016-03-15 20:12:37 -0500

    formatting

--- CSCE378/submit_correction.php
+++ CSCE378/submit_correction.php
@@ -8,41 +8,43 @@ html_top();
     <h1>Correction Form</h1>
   </div>
 </div>
-<div class="row">
-  <div class="col-md-12">
-    <div class="col-md-2">
-      <p>*Date:</p>
+<form role="form" action="" method="POST">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="col-md-2">
+        <p>*Date:</p>
+      </div>
+      <div class="col-md-4">
+        <input type="date" required>
+      </div>
     </div>
-    <div class="col-md-4">
-      <input type="date" required>
+    <div class="col-md-12">
+      <div class="col-md-2">
+        <p>*Duration:</p>
+      </div>
+      <div class="col-md-4">
+        <input type="date" required>
+      </div>
     </div>
-  </div>
-  <div class="col-md-12">
-    <div class="col-md-2">
-      <p>*Duration:</p>
+    <div class="col-md-12">
+      <div class="col-md-2">
+        <p>&nbsp;Comments:</p>
+      </div>
+      <div class="col-md-4">
+        <textarea rows="7"></textarea>
+      </div>
     </div>
-    <div class="col-md-4">
-      <input type="date" required>
-    </div>
-  </div>
-  <div class="col-md-12">
-    <div class="col-md-2">
-      <p>&nbsp;Comments:</p>
-    </div>
-    <div class="col-md-4">
-      <textarea rows="7"></textarea>
-    </div>
-  </div>
-  <div class="col-md-12">
-    <div class="col-md-2"></div>
-    <div class="col-md-1">
+    <div class="col-md-12">
+      <div class="col-md-2"></div>
+      <div class="col-md-1">
         <button type="submit" class="btn btn-default btnsm">Submit</button>
-    </div>
-    <div class="col-md-1">
+      </div>
+      <div class="col-md-1">
         <button type="submit" class="btn btn-default btnsm">Cancel</button>
+      </div>
     </div>
   </div>
-</div>
+</form>
 <div class="row">
   <div class="col-md-12">
     <br><p class="small">* is a required field</p>
