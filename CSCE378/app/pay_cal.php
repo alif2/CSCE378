01fd76ba507dce196e85892ff737acce59f90152
HelloDusty <mikejyf@gmail.com>
2016-03-25 13:54:01 -0500

    clear one

--- CSCE378/app/pay_cal.php
+++ CSCE378/app/pay_cal.php
@@ -11,25 +11,12 @@ function get_time_diff($s_UTC){
    return $s_work_hours;
 }
 
-function get_hours($s_date_time){       //get the hours from the single day and cal the hours without rand
-     
-
-      $s_date_diff = $s_date_time->diff($date);
-      $s_hours = $s_date_diff->h;
-      $s_mins = $s_date_diff->i;
-      $s_second = 
-      $s_hours = $s_hours + ($s_mins / 60.0 );
-
-      return $s_hours;
-   }
 
 function user_hours_get($i_user_id, $s_time_UTC){ //for example $i_user_id = 1; $s_time_UTC = "2016-03-26";
       $s_db_time = database_get_user_specific_time($i_user_id,$s_time_UTC);  //get the UTC_time from the database
       $s_working_hours = get_time_diff($s_db_time); //using func to get single day hours and mins
-      echo $s_working_hours;
-    //  $s_working_hours = get_hours($s_mydate);  //calculate to only hours
       return $s_working_hours;
    }
-   $i_user_id = 1; 
-   $s_time_UTC = "2016-03-26";
-   user_hours_get($i_user_id, $s_time_UTC);
+$i_user_id = 1; 
+$s_time_UTC = "2016-03-26";
+echo user_hours_get($i_user_id, $s_time_UTC);
