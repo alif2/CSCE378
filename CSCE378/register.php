c1288312c1e5861cf04fffcf8ecbeab41a4cbb44
Mengmei Ye <mengmeiye@gmail.com>
2016-04-13 21:45:44 -0500

    Design Updates

--- CSCE378/register.php
+++ CSCE378/register.php
@@ -5,19 +5,19 @@ html_top();
 
 <h3>Fixed Navbar</h3>
 <form role="form" action="app/register_user.php" method="post">
-    <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="text" class="form-control" id="email" name="email">
-    </div>
+  <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="text" class="form-control" id="email" name="email">
+  </div>
 
-    <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
+  <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" class="form-control" id="password" name="password">
+  </div>
 
-    <div class="form-group">
-        <button type="submit" class="form-control">Submit</button>
-    </div>
+  <div class="form-group">
+      <button type="submit" class="form-control">Submit</button>
+  </div>
 </form>
 
 <?php
