80c3a29ccb37ba9bd69514c5c00bb5a2bf48b5c1
HelloDusty <mikejyf@gmail.com>
2016-04-19 17:30:20 -0500

    show the work hours in the array by key names

--- CSCE378/app/user.php
+++ CSCE378/app/user.php
@@ -7,10 +7,11 @@ function add_work_hours($s_work_hours){
     $i_sum = 0;
 
     for($i = 0; $i < count($s_work_hours) - 1; $i += 2) {
-      $i_sum += strtotime($s_work_hours[$i + 1]) - strtotime($s_work_hours[$i]);
-   
+    $i_sum += strtotime($s_work_hours[$i + 1]) - strtotime($s_work_hours[$i]);
+    } 
+    
     return $i_sum /= 3600.0;
-}
+
 }
 
 # Get the total work hours for user for a given day
@@ -24,47 +25,60 @@ function user_get_hours_by_day($s_email, $s_date_utc){
 function user_get_hours_by_week($s_email, $s_start_date, $s_end_date){
 	$s_user_hours_dates = array();
 	$s_uesr_work_hours_array = array();
+    $s_uesr_temp_array = array();
     $s_user_UTC_week = database_user_get_time_tracking_events_by_day($s_email,$s_start_date, $s_end_date);
   
     $s_date = date('Y-m-d', strtotime($s_user_UTC_week[0][0]. '-2 hours'));
+    
+
+ 
 
     for($i=0; $i<count($s_user_UTC_week); $i++){
 
      
-    if(strpos($s_user_UTC_week[$i][0],$s_date ) === false){
+        if(strpos($s_user_UTC_week[$i][0],$s_date ) === false){
     
-    array_push($s_uesr_work_hours_array,add_work_hours($s_user_hours_dates));
+        
+            $s_uesr_temp_array[$s_date] = add_work_hours($s_user_hours_dates);
+            $s_uesr_work_hours_array = $s_uesr_work_hours_array + $s_uesr_temp_array;
+             $s_date = date('Y-m-d', strtotime($s_date. '+1 day'));
 
-     $s_date = date('Y-m-d', strtotime($s_date. '+1 day'));
 
-
-    while(strpos($s_user_UTC_week[$i][0],$s_date) === false){
+            while(strpos($s_user_UTC_week[$i][0],$s_date) === false){
     
 
-       #check if the next in the 
-    $s_date = date('Y-m-d', strtotime($s_date. '+1 day'));
-    array_push($s_uesr_work_hours_array,0); 
+            #check if the next in the 
+            $s_date = date('Y-m-d', strtotime($s_date. '+1 day'));
+         
     
-    }
+            }
     
-   $i--;
+             $i--;
 
-    unset($s_user_hours_dates);
-    $s_user_hours_dates = array();
-    }
+            unset($s_user_hours_dates);
+            $s_user_hours_dates = array();
+        }
 
 
-    else{
+    else{     #there is work hours in this days put the hours in the date_hours array
 	array_push($s_user_hours_dates,$s_user_UTC_week[$i][0]);
+       
     }
     
 
     }
 
-    array_push($s_uesr_work_hours_array,add_work_hours($s_user_hours_dates));
 
+    $s_uesr_temp_array[$s_date] = add_work_hours($s_user_hours_dates);
+            $s_uesr_work_hours_array = $s_uesr_work_hours_array + $s_uesr_temp_array;
     return $s_uesr_work_hours_array;
    }
 
-   print_r(user_get_hours_by_week('fdsfa','2016-03-10','2016-03-25'));
+   #print_r(user_get_hours_by_week('fdsfa','2016-03-01','2016-04-08'));
+
+
+
+
+
+
 
