97c1aef1f34ace5154108e04fad9ba1ec1d147b3
Adam Lif <adamlnk@neb.rr.com>
2016-04-19 18:11:45 -0500

    fix registration

--- CSCE378/app/register_user.php
+++ CSCE378/app/register_user.php
@@ -1,8 +1,8 @@
 <?php
 require_once('core.php');
 
-$s_email = $_POST['email'];
-$s_password = $_POST['password'];
+$s_email = $_POST['register-email'];
+$s_password = $_POST['register-password'];
 $s_salt = secure_create_salt();
 $s_pepper = file_read_pepper();
 $s_hash = secure_create_hash($s_password, $s_salt, $s_pepper);
