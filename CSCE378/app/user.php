b315db80d8a87aa63a8cdba8c89f3c577365da23
HelloDusty <mikejyf@gmail.com>
2016-04-19 20:30:01 -0500

    solve the bug for work over one day

--- CSCE378/app/user.php
+++ CSCE378/app/user.php
@@ -3,75 +3,88 @@ require_once('core.php');
 
 # Get sum of all ClockIn/ClockOut pairs in hours
 # s_work_hours is array
-function add_work_hours($s_work_hours){
+
+
+function add_work_hours($s_work_hours,$s_email){
     $i_sum = 0;
+    $i = 0;
+    $s_date = date('Y-m-d', strtotime($s_work_hours[0]. '-2 hours'));
+    $s_event_type = database_uesr_check_eventType($s_email,$s_work_hours[0]);
+    if($s_event_type == 'ClockOut'){
+        $i_sum += strtotime($s_work_hours[0]) - strtotime($s_date. '+2 hours');
+        $i++;
+    }
 
-    for($i = 0; $i < count($s_work_hours) - 1; $i += 2) {
-#<<<<<<< HEAD
-    $i_sum += strtotime($s_work_hours[$i + 1]) - strtotime($s_work_hours[$i]);
+    for(; $i < count($s_work_hours) - 1; $i += 2) {
+
+        $i_sum += strtotime($s_work_hours[$i + 1]) - strtotime($s_work_hours[$i]);
     } 
-    
-    return $i_sum /= 3600.0;
 
+    $s_event_type = database_uesr_check_eventType($s_email,$s_work_hours[count($s_work_hours) - 1]);
+    if($s_event_type == 'ClockIn'){
+        $i_sum += strtotime($s_date.'+1 day'.'+2 hour') - strtotime($s_work_hours[count($s_work_hours)-1]);
+    }
+    return $i_sum /= 3600.0;
+    
 
 }
+
 
 # Get the total work hours for user for given date range
 # $s_date_utc in format YYYY-MM-DD
 function user_get_hours_by_date_range($s_email, $s_start_date, $s_end_date){
 	$s_user_hours_dates = array();
-
-	$s_uesr_work_hours_array = array();
+    $s_uesr_work_hours_array = array();
     $s_uesr_temp_array = array();
     $s_user_UTC_week = database_user_get_time_tracking_events_by_date_range($s_email,$s_start_date, $s_end_date);
   
     $s_date = date('Y-m-d', strtotime($s_user_UTC_week[0][0]. '-2 hours'));
     
-
- 
-
-    for($i=0; $i<count($s_user_UTC_week); $i++){
+    for($i = 0; $i < count($s_user_UTC_week); $i++){
 
      
         if(strpos($s_user_UTC_week[$i][0],$s_date ) === false){
     
         
-            $s_uesr_temp_array[$s_date] = add_work_hours($s_user_hours_dates);
+            $s_uesr_temp_array[$s_date] = add_work_hours($s_user_hours_dates,$s_email);
             $s_uesr_work_hours_array = $s_uesr_work_hours_array + $s_uesr_temp_array;
-             $s_date = date('Y-m-d', strtotime($s_date. '+1 day'));
-
-
+            $s_date = date('Y-m-d', strtotime($s_date. '+1 day'));
+ 
+            
             while(strpos($s_user_UTC_week[$i][0],$s_date) === false){
-    
-
             #check if the next in the 
             $s_date = date('Y-m-d', strtotime($s_date. '+1 day'));
-         
-    
             }
-    
-             $i--;
 
             unset($s_user_hours_dates);
             $s_user_hours_dates = array();
+
+            $event_type = database_uesr_check_eventType($s_email,$s_user_UTC_week[$i][0]);
+         
+            $i--;
+            
         }
 
 
-    else{     #there is work hours in this days put the hours in the date_hours array
-	array_push($s_user_hours_dates,$s_user_UTC_week[$i][0]);
-       
-    }
+        else{     #there is work hours in this days put the hours in the date_hours array
+	        array_push($s_user_hours_dates,$s_user_UTC_week[$i][0]);
+        }
     
 
     }
 
 
-    $s_uesr_temp_array[$s_date] = add_work_hours($s_user_hours_dates);
-            $s_uesr_work_hours_array = $s_uesr_work_hours_array + $s_uesr_temp_array;
+    $s_uesr_temp_array[$s_date] = add_work_hours($s_user_hours_dates,$s_email);
+    $s_uesr_work_hours_array = $s_uesr_work_hours_array + $s_uesr_temp_array;
     return $s_uesr_work_hours_array;
-   }
+} 
+   #print_r((strtotime('2016-03-10T23:48:19.119Z') - strtotime('2016-03-10'.'+1 hour'))/3600);
+    # print_r((strtotime('2016-03-10'.'+1 day'.'+1 hour') - strtotime('2016-03-10T23:48:19.119Z') )/3600);
+  $s_event_type = database_uesr_check_eventType('fdsfa','2016-04-07T01:52:19.594Z');
+#print_r($s_event_type);
+  
 
-   print_r(user_get_hours_by_date_range('fdsfa','2016-03-01','2016-04-08'));
+   print_r(user_get_hours_by_date_range('fdsfa','2015-09-01','2016-04-08'));
 
 
 
