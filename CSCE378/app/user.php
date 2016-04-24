d1368b85f70f07e419835fceb283ba2b10bc00c8
Adam Lif <adamlnk@neb.rr.com>
2016-04-24 14:33:30 -0500

    remove register.php

--- CSCE378/app/user.php
+++ CSCE378/app/user.php
@@ -39,16 +39,12 @@ function user_get_sum_hours_by_date_range($s_email, $s_start_date, $s_end_date) 
 # $s_date_utc in format YYYY-MM-DD
 function user_get_hours_by_date_range($s_email, $s_start_date, $s_end_date){
 	$s_user_hours_dates = array();
-
-
     $s_user_work_hours_array = array();
     $s_user_temp_array = array();
     $s_user_UTC_week = database_user_get_time_tracking_events_by_date_range($s_email,$s_start_date, $s_end_date);
 
-
     if($s_user_UTC_week == NULL) {
-        $emety = array();
-        return $emety;
+        return array();
     }
     if(database_user_check_event_type($s_email,$s_user_UTC_week[count($s_user_UTC_week)-1][0]) == 'ClockIn') {
         array_pop($s_user_UTC_week);
@@ -57,7 +53,6 @@ function user_get_hours_by_date_range($s_email, $s_start_date, $s_end_date){
     $s_date = date('Y-m-d', strtotime($s_user_UTC_week[0][0]. '-2 hours'));
     
     for($i = 0; $i < count($s_user_UTC_week); $i++){
-
         if(strpos($s_user_UTC_week[$i][0],$s_date ) === false){
             $s_user_temp_array[$s_date] = add_work_hours($s_user_hours_dates,$s_email);
             $s_user_work_hours_array = $s_user_work_hours_array + $s_user_temp_array;
@@ -72,7 +67,6 @@ function user_get_hours_by_date_range($s_email, $s_start_date, $s_end_date){
             $s_user_hours_dates = array();
 
             $event_type = database_user_check_event_type($s_email,$s_user_UTC_week[$i][0]);
-         
             $i--;
 
         } else {     #there is work hours in this days put the hours in the date_hours array
@@ -81,9 +75,5 @@ function user_get_hours_by_date_range($s_email, $s_start_date, $s_end_date){
     }
  
     $s_user_temp_array[$s_date] = add_work_hours($s_user_hours_dates, $s_email);
-    $s_user_work_hours_array = $s_user_work_hours_array + $s_user_temp_array;
-    return $s_user_work_hours_array;
-} 
-
-     print_r(user_get_hours_by_date_range('fdsfa','2013-09-01','2016-04-07'));
-
+    return $s_user_work_hours_array + $s_user_temp_array;
+}
